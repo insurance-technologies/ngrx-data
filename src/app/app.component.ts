075bdc7c9cdd532097b6f9aa62e59cbcc1e135fe
Alejandro Guardiola <aleguardiolam@gmail.com>
2019-04-10 15:57:47 -0400

    Updates in the npm package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from 'ngrx-data-lib';
-import { UserService } from 'src/services/userService';
-import { Store } from '@ngrx/store';
-import { AddEntityToState } from 'projects/ngrx-data-lib/src/public_api';
-import { Observable } from 'rxjs';
 import { ImmutableObservable } from 'ngrx-data-lib/lib/helpers/immutable-observable';
 import { User } from 'src/models/user';
+import { UserService } from 'src/services/userService';
 
 @Component({
-  selector: 'ngd-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
   
-  title = 'ngrx-data';
+  title = 'testingLib';
+
   users$: ImmutableObservable<User[]>;
 
-  constructor(private userService: UserService, private dataService: DataService, private store: Store<any>)
+  constructor(private userService: UserService)
   {
-     
-  } 
 
-  ngOnInit(): void {   
-
-     this.users$ = this.userService.select();
-     
   }
 
-  onClick()
-  {    
-    this.userService.dispatch(this.dataService.GET);
+  ngOnInit(): void {    
+    this.users$ = this.userService.select();    
+  }
+
+  onGetUsers() : void{
+    this.userService.dispatch(this.userService.GET);
+  }
+
+  onReset() : void{
+    this.users$.reset();
+  }
+
+  onSave(user: User) : void {    
+    
+    this.userService.dispatchUpdateEntity(user);
+         
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule,
     StoreModule.forRoot(NgrxDataLibModule.Reducer),
     NgrxDataLibModule.forRoot({
